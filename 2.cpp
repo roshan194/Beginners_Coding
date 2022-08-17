340b26225cfac7ade14cdb4808c192a41745af51
Roshan <71066704+roshan194@users.noreply.github.com>
2022-08-17 11:23:58 +0530

    final

---
+++ 2.cpp
@@ -0,0 +1,12 @@
+//Write a Program to Print Integer Number Entered by User
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int number;
+    cout<<"Enter a Number";
+    cin>>number;
+    cout<<"Your Entered Number is" <<number;
+    return 0;
+}
