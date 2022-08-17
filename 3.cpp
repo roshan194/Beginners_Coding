d8f37cd32d281f2ec918a531348729812ae8e262
Roshan <71066704+roshan194@users.noreply.github.com>
2022-08-17 11:23:10 +0530

    final

---
+++ 3.cpp
@@ -0,0 +1,15 @@
+//Write a Program to Add Two Integer Numbers Entered by User
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int N1, N2, sum;
+    cout<<"Enter First Number";
+    cin>>N1;
+    cout<<"Enter Second Number";
+    cin>>N2;
+    sum = N1 + N2;
+    cout<<"Sum of Two Number is " <<sum;
+    return 0;
+}
