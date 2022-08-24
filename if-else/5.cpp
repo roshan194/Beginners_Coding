3c22e27e27699e1d7279ba649d703f0b5583da00
Roshan <71066704+roshan194@users.noreply.github.com>
2022-08-24 09:48:20 +0530

    final

---
+++ if-else/5.cpp
@@ -0,0 +1,15 @@
+// Program to Calculate Sum of first N Natural Numbers
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, sum = 0;
+    cout << "Enter a positive integer: ";
+    cin >> n;
+    for (int i = 1; i <= n; ++i)
+    {
+        sum += i;
+    }
+    cout << "Sum = " << sum;
+    return 0;
+}
