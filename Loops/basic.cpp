a9eb33b761c85b960787a1364cb48adc8b4c6b21
Roshan <71066704+roshan194@users.noreply.github.com>
2022-08-26 11:03:37 +0530

    final

---
+++ Loops/basic.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    int sum = 0;
+    cout<<"Enter number";
+    cin >> n;
+    for(int i = 1;i<=n;i++){
+        sum = sum + i;
+    }
+    cout<<sum;
+    return 0;
+}
