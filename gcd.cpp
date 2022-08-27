172af1954218a969319c0cba1599aa24768254f4
Roshan <71066704+roshan194@users.noreply.github.com>
2022-08-27 08:37:00 +0530

    final

--- gcd.cpp
+++ gcd.cpp
@@ -1,33 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+int main() {
 
-int gcd(int a, int b){
-    if(a == 0){
-        return b;
+    int n1, n2, hcf;
+    cout << "Enter two numbers: ";
+    cin >> n1 >> n2;
+    // swapping variables n1 and n2 if n2 is greater than n1.
+    if ( n2 > n1) {
+        int temp = n2;
+        n2 = n1;
+        n1 = temp;
     }
+    for (int i = 1; i <= n2; ++i) {
 
-    if(b == 0){
-        return a;
-    }
-
-    while(a!=b){
-        if(a>b){
-            a = a - b;
+        if (n1 % i == 0 && n2 % i ==0) {
+        hcf = i;
         }
-        else{
-            b = b -a;
-        }
     }
 
-    return a;
-}
-
-int main (){
-    int a, b;
-    cout << "Enter the values of a & b" << endl;
-    cin>>a>>b;
-    int ans = gcd(a,b);
-    cout << "The GCD of" << a << " & " << b  <<  "is :" << ans << endl;
-
+    cout << "HCF = " << hcf;
     return 0;
-}
+}    
